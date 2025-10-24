2d91c893854eb89d434de6c179fd33add7936b23
gabrielsouzaad <gabrielsouzairru@gmail.com>
2025-10-24 14:26:16 -0300

    Implentação do encontrarPorId e correção de alguns erros

--- src/model/dao/DAOFactory.java
+++ src/model/dao/DAOFactory.java
@@ -1,5 +1,11 @@
 package model.dao;
 
+import db.DB;
+import model.dao.impl.VendedorDAOJDBC;
+
 public class DAOFactory {
 
+	public static VendedorDAO criarVendedorDAO() {
+		return new VendedorDAOJDBC(DB.getConnection());
+	}
 }
--- src/model/dao/impl/VendedorDAOJDBC.java
+++ src/model/dao/impl/VendedorDAOJDBC.java
@@ -1,12 +1,26 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.VendedorDAO;
+import model.entities.Departamento;
 import model.entities.Vendedor;
 
 public class VendedorDAOJDBC implements VendedorDAO {
 
+	
+	private Connection conn;
+	
+	public VendedorDAOJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void inserir(Vendedor obj) {
 		// TODO Auto-generated method stub
@@ -27,9 +41,45 @@ public class VendedorDAOJDBC implements VendedorDAO {
 
 	@Override
 	public Vendedor encontrarPorId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	    PreparedStatement st = null;
+	    ResultSet rs = null;
+	    try {
+	        st = conn.prepareStatement(
+	            "SELECT seller.*, department.Name AS DepName "
+	            + "FROM seller "
+	            + "INNER JOIN department "
+	            + "ON seller.DepartmentId = department.Id "
+	            + "WHERE seller.Id = ?"
+	        );
+	        
+	        st.setInt(1, id);
+	        rs = st.executeQuery();
+	        
+	        if (rs.next()) {
+	            Departamento dep = new Departamento();
+	            dep.setId(rs.getInt("DepartmentId"));
+	            dep.setNome(rs.getString("DepName"));
+	            
+	            Vendedor obj = new Vendedor();
+	            obj.setId(rs.getInt("Id"));
+	            obj.setNome(rs.getString("Name"));
+	            obj.setEmail(rs.getString("Email"));
+	            obj.setSalariobase(rs.getDouble("BaseSalary"));
+	            obj.setDataDeNascimento(rs.getDate("BirthDate"));
+	            obj.setDepartamento(dep);
+	            
+	            return obj;
+	        }
+	        return null;
+	        
+	    } catch (SQLException e) {
+	        throw new DbException(e.getMessage());
+	    } finally {
+	        DB.closeStatement(st);
+	        DB.closeResultSet(rs);
+	    }
 	}
+
 
 	@Override
 	public List<Vendedor> encontrarTodos() {
