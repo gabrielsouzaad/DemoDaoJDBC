d902e975668337736b410bff8248a6609cebd960
gabrielsouzaad <gabrielsouzairru@gmail.com>
2025-10-25 16:58:51 -0300

    Implementação dos métodos para inserir, atualizar, deletar, encontrarPorId, encontrarTodos. Código finalizado!

--- src/application/Programa2.java
+++ src/application/Programa2.java
@@ -1,16 +1,47 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DAOFactory;
 import model.dao.DepartamentoDAO;
+import model.entities.Departamento;
 
 public class Programa2 {
 
 	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
 		
 		DepartamentoDAO departamentoDAO = DAOFactory.criarDepartamentoDAO();
-		
-		
 
+		System.out.println("--- Teste número 1: encontrarPorId ---");
+		Departamento dep = departamentoDAO.encontrarPorId(1);
+		System.out.println(dep);
+		
+		System.out.println("\n--- Teste número 2: encontrarTodos ---");
+		List<Departamento> list = departamentoDAO.encontrarTodos();
+		for (Departamento d : list) {
+			System.out.println(d);
+		}
+
+		System.out.println("\n--- Teste número 3: departamento inserir ---");
+		Departamento newDepartment = new Departamento(null, "Music");
+		departamentoDAO.inserir(newDepartment);
+		System.out.println("Inserido! Novo Id: " + newDepartment.getId());
+
+		System.out.println("\n--- Teste número 4: departamento atualizar ---");
+		Departamento dep2 = departamentoDAO.encontrarPorId(1);
+		dep2.setNome("Food");
+		departamentoDAO.atualizar(dep2);
+		System.out.println("Atualização completa!!!");
+		
+		System.out.println("\n--- Teste número 5: departamento deletado ---");
+		System.out.print("Entre com o id para ser deletado: ");
+		int id = sc.nextInt();
+		departamentoDAO.deletarDoId(id);
+		System.out.println("Deletado com sucesso!!!");
+
+		sc.close();
 	}
-
 }
--- src/model/dao/impl/DepartamentoDAOJDBC.java
+++ src/model/dao/impl/DepartamentoDAOJDBC.java
@@ -5,84 +5,149 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
 import db.DbException;
+import db.DbIntegrityException;
 import model.dao.DepartamentoDAO;
 import model.entities.Departamento;
 
 public class DepartamentoDAOJDBC implements DepartamentoDAO {
-	
+
 	private Connection conn;
 	
 	public DepartamentoDAOJDBC(Connection conn) {
 		this.conn = conn;
 	}
 	
+	@Override
+	public Departamento encontrarPorId(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Departamento obj = new Departamento();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Name"));
+				return obj;
+			}
+			return null;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+
+	@Override
+	public List<Departamento> encontrarTodos() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+				"SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+
+			List<Departamento> list = new ArrayList<>();
+
+			while (rs.next()) {
+				Departamento obj = new Departamento();
+				obj.setId(rs.getInt("Id"));
+				obj.setNome(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 
 	@Override
 	public void inserir(Departamento obj) {
-		
 		PreparedStatement st = null;
-		
 		try {
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-					+ "(Id, Name ) "
-					+ "VALUES "
-					+ "(?, ?) ",
-					Statement.RETURN_GENERATED_KEYS);
-			
-			st.setInt(1, obj.getId());
-			st.setString(2, obj.getNome());
+				"INSERT INTO department " +
+				"(Name) " +
+				"VALUES " +
+				"(?)", 
+				Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getNome());
+
 			int rowsAffected = st.executeUpdate();
+			
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
 				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
-				
-				DB.closeResultSet(rs);
 			}
 			else {
-				throw new DbException("Nenhuma linha foi afetada!!!");
+				throw new DbException("Erro! Nenhuma linha afetada!!!");
 			}
-					
-		} catch (SQLException e) {
-			throw new DbException(e.getMessage());
-		} finally {
-			DB.closeStatement(st);
-			
 		}
-		
-		
-		
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void atualizar(Departamento obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"UPDATE department " +
+				"SET Name = ? " +
+				"WHERE Id = ?");
+
+			st.setString(1, obj.getNome());
+			st.setInt(2, obj.getId());
+
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deletarDoId(Integer id) {
-		// TODO Auto-generated method stub
-		
-	}
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+				"DELETE FROM department WHERE Id = ?");
 
-	@Override
-	public Departamento encontrarPorId(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+			st.setInt(1, id);
 
-	@Override
-	public List<Departamento> encontrarTodos() {
-		// TODO Auto-generated method stub
-		return null;
+			st.executeUpdate();
+		}
+		catch (SQLException e) {
+			throw new DbIntegrityException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
 	}
-
 }
